678fbcaca5e284f28a553b263396f2658a458a99
FELIPE DE OLIVEIRA FLORENCIO <22282108@CFP127ALUNO.LOCAL>
2023-01-26 11:08:33 -0300

    online e webpack aprendidos

---
+++ Felipe Florencio/testes/script.js
@@ -0,0 +1,6 @@
+var sticky = UIkit.sticky('#sticky', {
+    offset: 50
+});
+var botaoDeTeste = document.getElementsByName('uk-button uk-button-default');
+const corpo = document.getElementsByClassName('body');
+document.appendChild()
